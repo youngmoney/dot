d5618e9eaeddea7f3a69d2c9b6fbe61cbb085a3f
Taylor L Money <git@taylorlmoney.com>
2020-06-17 20:12:49 -0700

    remove use of status to start session

--- commands/session/session/cli.py
+++ commands/session/session/cli.py
@@ -4,6 +4,7 @@ from . import client, server
 import base64
 import sys
 
+
 def debug(s):
     if True:
         if isinstance(s, str):
@@ -11,17 +12,22 @@ def debug(s):
         else:
             print((s()))
 
+
 def parse_and_run():
     parser = ArgumentParser(description="Execute Markdown Code Blocks")
     subparsers = parser.add_subparsers()
 
-    server_parser = subparsers.add_parser('server')
-    server_parser.add_argument('verb', choices=['start'], nargs=1)
+    server_parser = subparsers.add_parser(
+        "server", help="Start the server. It may be helpful to run in under (z)daemon.",
+    )
+    server_parser.add_argument("verb", choices=["start"], nargs=1)
     server_parser.set_defaults(func=run_server)
 
-    client_parser = subparsers.add_parser('client')
-    client_parser.add_argument('verb', choices=['start','execute', 'stop', 'kill'], nargs=1)
-    client_parser.add_argument('filename')
+    client_parser = subparsers.add_parser("client")
+    client_parser.add_argument(
+        "verb", choices=["start", "execute", "stop", "kill"], nargs=1
+    )
+    client_parser.add_argument("filename")
     client_parser.set_defaults(func=run_client)
     args = parser.parse_args()
 
@@ -35,16 +41,17 @@ def parse_and_run():
 def run_server(args):
     server.app.run(port=9009, threaded=True)
 
+
 def run_client(args):
     session = base64.urlsafe_b64encode(args.filename)
-    rs = client.RemoteSession(session, 'localhost', 9009)
+    rs = client.RemoteSession(session, "localhost", 9009)
     verb = args.verb[0]
-    if verb == 'execute':
+    if verb == "execute":
         lines = sys.stdin.readlines()
-        lines = [l.rstrip('\n') for l in lines]
+        lines = [l.rstrip("\n") for l in lines]
         output = rs.execute(lines, auto_start=True)
-        output = [o+'\n' for o in output]
+        output = [o + "\n" for o in output]
         sys.stdout.writelines(output)
     else:
         if not getattr(rs, verb)():
-            raise ValueError('Error: %s did not succeed.' % args.verb)
+            raise ValueError("Error: %s did not succeed." % args.verb)
